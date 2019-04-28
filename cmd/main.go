48eff767079c89d35a1abb2353e07346df16c1c9
Ludin Nento <ludyyn@gmail.com>
2019-04-28 18:44:41 +0800

    change main file

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/fn-code/gogi/internal/view"
+	"github.com/fn-code/gogi/internal/web/server"
+)
+
+func main() {
+	view.InitTemplate("./templates", ".gohtml")
+
+	fmt.Println("Server is running")
+	srv, err := server.New(":8888", "public")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = srv.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer srv.Close()
 }
